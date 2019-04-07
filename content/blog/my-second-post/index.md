bb71424ca7103cdcd9890c5876152cd8ce5f34d3
Kellen Mace <kellen.mace@gmail.com>
2019-04-06 23:37:40 -0400

    Initial commit

---
+++ content/blog/my-second-post/index.md
@@ -0,0 +1,25 @@
+---
+title: My Second Post!
+date: "2015-05-06T23:46:37.121Z"
+---
+
+Wow! I love blogging so much already.
+
+`gist:bf86ba5d19557c00c7171b5581323158`
+
+Did you know that "despite its name, salted duck eggs can also be made from
+chicken eggs, though the taste and texture will be somewhat different, and the
+egg yolk will be less rich."?
+([Wikipedia Link](http://en.wikipedia.org/wiki/Salted_duck_egg))
+
+Yeah, I didn't either.
+
+<p>WordPress’&nbsp;<code>get_super_admins()</code> function can be used to get the logins for all the users on the current site. Its return value is in array that looks like: <code>[ ‘maryann123’, ‘johnsmith456’, ‘annie-mcarthur’ ]</code>. If you want <code>WP_User</code> objects for each of those users though, you can use <code>get_users()</code>&nbsp;function and pass in&nbsp;<code>get_super_admins()</code>&nbsp;as the <code>‘login__in</code>‘ argument, like this:</p>
+
+`gist:bf86ba5d19557c00c7171b5581323158`
+
+<h2>Get non-Super Admins</h2>
+<p>If you want to do the opposite and get all non-Super Admins, you can use <code>‘login__not_in’</code> instead, like this:</p>
+
+`gist:bf86ba5d19557c00c7171b5581323158`
+									
---
+++ src/components/layout.js
@@ -0,0 +1,78 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import "./layout.css"
+import { rhythm, scale } from "../utils/typography"
+
+class Layout extends React.Component {
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+    let header
+
+    if (location.pathname === rootPath) {
+      header = (
+        <h1
+          style={{
+            ...scale(1.5),
+            marginBottom: rhythm(1.5),
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h1>
+      )
+    } else {
+      header = (
+        <h3
+          style={{
+            fontFamily: `Montserrat, sans-serif`,
+            marginTop: 0,
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: `none`,
+              textDecoration: `none`,
+              color: `inherit`,
+            }}
+            to={`/`}
+          >
+            {title}
+          </Link>
+        </h3>
+      )
+    }
+    return (
+      <div style={{ background: `linear-gradient(90deg,#f7c626 15%,#f68c2f 40%,#e5127d 85%)`, paddingTop: `3px` }}>
+        <div style={{ background: `#292829` }}>
+          <div
+            style={{
+              marginLeft: `auto`,
+              marginRight: `auto`,
+              maxWidth: rhythm(24),
+              padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+            }}
+          >
+            <header>{header}</header>
+            <main>{children}</main>
+            <footer>
+              © {new Date().getFullYear()} Kellen Mace
+            </footer>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Layout
