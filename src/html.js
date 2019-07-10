0725ce4e19f76aa0234811f250ea796e6b61a6b7
Kellen Mace <kellen.mace@gmail.com>
2019-07-09 22:32:40 -0400

    Rejigger AdSense

--- src/html.js
+++ src/html.js
@@ -11,10 +11,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <script
-          async
-          src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
